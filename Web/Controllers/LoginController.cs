419b6eba36d6963fa8deabcddb7562c79e6ff6c2
Esteban Chamber <snapes@gmail.com>
2022-11-12 19:22:15 -0600

    login controller added, sweet read only deleted

---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,87 @@
+﻿using ApplicationCore.Services;
+using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Web.Utils;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        //public ActionResult Index(Usuario usuario)
+        public ActionResult Login(Usuario usuario)
+        {
+            IServiceUsuario _ServiceUsuario = new ServiceUsuario();
+            Usuario oUsuario = null;
+            try
+            {
+                ModelState.Remove("Nombre");
+                ModelState.Remove("IdTipoUsuario");
+                if (ModelState.IsValid)
+                {
+                    oUsuario = _ServiceUsuario.GetUsuario(usuario.Correo, usuario.Contraseña);
+                    if (oUsuario != null)
+                    {
+                        Session["User"] = oUsuario;
+                        Log.Info($"Accede{oUsuario.Nombre}" +
+                            $"con el rol {oUsuario.TipoUsuario.Id}");
+                   //     TempData["mensaje"] = Util.SweetAlertHelper.Mensaje("Login",
+                     //       "Usuario autenticado", Utils.SweetAlertMessageType.success);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        Log.Warn($"Intento de inicio de secion{usuario.Correo}");
+                        //ViewBag.NotificationMessage = Util.SweetAlertHelper.Mensaje("Login",
+//"Usuario no válido", Util.SweetAlertMessageType.warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+            return View("Index");
+        }
+        public ActionResult UnAuthorized()
+        {
+            ViewBag.Message = "No autorizado";
+            if (Session["User"] != null)
+            {
+                Usuario usuario = Session["User"] as Usuario;
+                Log.Warn($"No autorizado {usuario.Correo}");
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            try
+            {
+                Session["User"] = null;
+                return RedirectToAction("Index", "Login");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+    }
+}

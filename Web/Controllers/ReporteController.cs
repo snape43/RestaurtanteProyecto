14c516bdf2495fc07602b714953e2f0b3090118f
Ale <alegon26@gmail.com>
2022-11-28 00:31:41 -0600

    Backend: crear reporte1

---
+++ Web/Controllers/ReporteController.cs
@@ -0,0 +1,75 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using Infraestructure.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Web.Security;
+using Web.ViewModel;
+
+namespace Web.Controllers
+{
+    public class ReporteController : Controller
+    {
+        // GET: Reporte
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [CustomAuthorize((int)Roles.Administrador)]
+        public ActionResult RptVentasFecha()
+        {
+
+            return View();
+
+        }
+        public ActionResult DescargarReporteVentasFecha(String fechaInicial, String fechafinal)
+        {
+            try
+            {
+                var rpt = new ReportClass();
+                rpt.FileName = Server.MapPath("/Reports/ReporteVentas.rpt");
+                rpt.Load();
+
+                rpt.SetParameterValue("PIfecha", fechaInicial);
+                rpt.SetParameterValue("PFfecha", fechafinal);
+
+                // Report connection
+                var connInfo = CrystalReportsRest.GetConnectionInfo();
+                TableLogOnInfo logonInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = rpt.Database.Tables;
+                foreach (Table table in tables)
+                {
+                    logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+                }
+
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                Stream stream = rpt.ExportToStream(ExportFormatType.PortableDocFormat);
+                rpt.Dispose();
+                rpt.Clone();
+                return new FileStreamResult(stream, "application.pdf");
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+
+        }
+    }
+}
---
+++ Web/ViewModel/CrystalReportsRest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModel
+{
+    public class CrystalReportsRest
+    {
+        public static CrystalDecisions.Shared.ConnectionInfo GetConnectionInfo()
+        {
+            var SConn = new System.Data.SqlClient.SqlConnectionStringBuilder(System.Configuration.ConfigurationManager.ConnectionStrings["DBRestauranteCnn"].ConnectionString);
+
+            CrystalDecisions.Shared.ConnectionInfo connInfo = new CrystalDecisions.Shared.ConnectionInfo();
+            connInfo.ServerName = SConn.DataSource;
+            connInfo.DatabaseName = SConn.InitialCatalog;
+            connInfo.UserID = SConn.UserID;
+            connInfo.Password = SConn.Password;
+            //    connInfo.IntegratedSecurity = SConn.IntegratedSecurity;
+
+
+            /* CrystalDecisions.Shared.ConnectionInfo connInf = new CrystalDecisions.Shared.ConnectionInfo();
+             connInf.ServerName = @"DESKTOP-6VL71VD";
+             connInf.DatabaseName = "DBRestaurante";
+             connInfo.IntegratedSecurity = true; */
+
+            return connInfo;
+        }
+
+
+    }
+}

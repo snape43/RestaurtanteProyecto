67837a5102caba925db6bff1b90fe9d2d37d46f7
Ale <alegon26@gmail.com>
2022-11-28 12:40:15 -0600

    Backend: crear reporte2

--- Web/Controllers/ReporteController.cs
+++ Web/Controllers/ReporteController.cs
@@ -1,5 +1,6 @@
 ﻿using CrystalDecisions.CrystalReports.Engine;
 using CrystalDecisions.Shared;
+using Infraestructure.Models;
 using Infraestructure.Utils;
 using System;
 using System.Collections.Generic;
@@ -71,5 +72,66 @@ namespace Web.Controllers
             }
 
         }
+
+
+        [CustomAuthorize((int)Roles.Administrador)]
+        public ActionResult RptVentasPago()
+        {
+
+            return View();
+
+        }
+        public ActionResult DescargarReporteVentasPago(String fechaInicial, String fechafinal, string tipopago)
+        {
+            try
+            {
+                var rpt = new ReportClass();
+                rpt.FileName = Server.MapPath("/Reports/ReporteVentasTipo.rpt");
+                rpt.Load();
+
+                rpt.SetParameterValue("PIfecha", fechaInicial);
+                rpt.SetParameterValue("PFfecha", fechafinal);
+                rpt.SetParameterValue("Ppago", tipopago);
+
+                // Report connection
+                var connInfo = CrystalReportsRest.GetConnectionInfo();
+                TableLogOnInfo logonInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = rpt.Database.Tables;
+                foreach (Table table in tables)
+                {
+                    logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+                }
+
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                Stream stream = rpt.ExportToStream(ExportFormatType.PortableDocFormat);
+                rpt.Dispose();
+                rpt.Clone();
+                return new FileStreamResult(stream, "application.pdf");
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+
+        }
+
+        public JsonResult GetTipoPago()
+        {
+            MyContext ctx = new MyContext();
+            var cats = ctx.TipoPago.Select(x => x.TipoPago1).ToList();
+            return Json(new { resultado = cats }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
